be3776d2b6d7eed18b5ad845aa5159e3fb31dec5
Max Stupple <max@stupple.org>
2019-10-07 12:28:02 +0100

    Add prototype Task model

--- MyTime/mysite/polls/models.py
+++ MyTime/mysite/polls/models.py
@@ -32,3 +32,20 @@ class Number(models.Model):
 
     def __str__(self):
         return str(self.value)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    descrition = models.CharField(max_length=1000)
+    due_date = models.DateField('due date')
+    time_estimate = models.IntegerField()
+    done = False
+
+    def __str__(self):
+        return self.title
+
+    def is_overdue(self):
+        return self.due_date < timezone.now()
+
+    def mark_as_done(self):
+        self.done = True
