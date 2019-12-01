9c862f5f67aa8e5210ea22c87291b66dabfd08d4
Max Stupple <max@stupple.org>
2019-12-01 21:42:06 +0000

    Add new model Routine

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 from django.utils import timezone
-import datetime
 
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
-    due_date = models.DateField('due date', default=timezone.now)
-    due_time = models.TimeField('due time', default=timezone.now)
-    time_estimate = models.DurationField('time estimate',
-                                         default=datetime.timedelta(minutes=0))
-    priority = models.IntegerField("priority", default=1)
+    due_date = models.DateTimeField('due date')
+    time_estimate = models.DurationField('time estimate')
     done = models.BooleanField(default=False)
 
     def __str__(self):
@@ -28,19 +24,17 @@ class Task(models.Model):
     def get_absolute_url(self):
         return f"/tasks/{self.id}/"
 
-<<<<<<< HEAD
-
-class Event(models.Model):
-    title = models.CharField(max_length=200)
-    start_time = models.DateField("start time")
-    finish_time = models.DateField("finish time")
-=======
 class Event(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField("date")
     start_time = models.TimeField("start time")
     end_time = models.TimeField("end time")
-    
+
     def __str__(self):
         return self.title
->>>>>>> origin/ft-scheduler
+
+
+class Routine(models.Model):
+    day = models.CharField(max_length=200)
+    start_time = models.TimeField("start time")
+    end_time = models.TimeField("end time")
