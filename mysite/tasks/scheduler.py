b56c69696902bb00be2355e2ecc82125b5451c17
Max Stupple <max@stupple.org>
2019-12-01 22:23:55 +0000

    Add class TimeChunk to scheduler

--- mysite/tasks/scheduler.py
+++ mysite/tasks/scheduler.py
@@ -1,3 +1,10 @@
+import datetime
 from django import timezone
-from .models import Task, Event
+from .models import Task, Event, Routine
 
+
+class TimeChunk():
+    def __init__(self, obj):
+        self.date = obj.get_day()
+        self.start = datetime.combine(date=self.date, time=obj.get_start())
+        self.end = datetime.combine(date=self.date, time=obj.get_end())
