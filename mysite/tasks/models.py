0d67f28e513c08e393f5a86dcbe166698cd1758d
Max Stupple <max@stupple.org>
2019-12-02 12:12:17 +0000

    Getters for Event and Routine models, continuing work on scheduler

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -29,13 +29,14 @@ class Task(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField("date")
+    day = None
     start_time = models.TimeField("start time")
     end_time = models.TimeField("end time")
 
     def __str__(self):
         return self.title
 
-    def get_day(self):
+    def get_date(self):
         return self.date
 
     def get_start(self):
@@ -44,20 +45,19 @@ class Event(models.Model):
     def get_end(self):
         return self.end_time
 
+    def get_name(self):
+        return self.title
 
-class Routine(models.Model):
-    day = models.IntegerField()
-    start_time = models.TimeField("start time")
-    end_time = models.TimeField("end time")
 
-    def get_day(self):
+class Routine(Event):
+    date = None
+    day = models.IntegerField("day")
+
+    def get_date(self):
         today = datetime.today()
         delta = (self.day - today.weekday()) % 7
         date = datetime.today() + timedelta(days=delta)
         return date
 
-    def get_start(self):
-        return self.start_time
-
-    def get_end(self):
-        return self.end_time
+    def get_day(self):
+        return self.day
--- mysite/tasks/scheduler.py
+++ mysite/tasks/scheduler.py
@@ -5,6 +5,26 @@ from .models import Task, Event, Routine
 
 class TimeChunk():
     def __init__(self, obj):
+        self.name = obj.get_name()
         self.date = obj.get_day()
         self.start = datetime.combine(date=self.date, time=obj.get_start())
         self.end = datetime.combine(date=self.date, time=obj.get_end())
+
+
+class AvailableTime(TimeChunk):
+    name = "free time"
+
+
+def make_schedule(day):
+    routine = Routine.objects.filter(day=day)[0]
+    RoutineTC = TimeChunk(routine)
+    date = RoutineTC.date
+    events = Event.objects.filter(date=date).order_by("-start_time")
+
+    event_tc_list = []
+    for event in events:
+        EventTC = TimeChunk(Event)
+        event_tc_list.append(EventTC)
+
+    tasks = Task.objects.filter(done=False).order_by("-due_date", "-time_estimate")
+
