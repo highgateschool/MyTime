b8f10a7f2d360a98da9ad0a4b2e13dc162efecef
Max Stupple <max@stupple.org>
2019-12-30 17:54:03 +0000

    The scheduler saga continues

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -9,6 +9,7 @@ class Task(models.Model):
     due_date = models.DateTimeField('due date')
     time_estimate = models.DurationField('time estimate')
     done = models.BooleanField(default=False)
+    override_routine = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -27,11 +28,13 @@ class Task(models.Model):
 
 
 class Event(models.Model):
+    purpose = models.CharField(max_length=200, default="event")
     title = models.CharField(max_length=200)
     date = models.DateField("date")
     day = None
     start_time = models.TimeField("start time")
     end_time = models.TimeField("end time")
+    override_routine = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -48,16 +51,38 @@ class Event(models.Model):
     def get_name(self):
         return self.title
 
+    def does_clash(self, other):
+        if self.start_time < other.end_time and self.end_time > other.start:
+            return True
+        else:
+            return False
 
-class Routine(Event):
-    date = None
+
+#class Routine(Event):
+#    purpose = models.CharField(max_length=200, default="routine")
+#    date = None
+#    day = models.IntegerField("day")
+#
+#    def get_date(self):
+#        today = datetime.today()
+#        delta = (self.day - today.weekday()) % 7
+#        date = datetime.today() + timedelta(days=delta)
+#        return date
+#
+#    def get_day(self):
+#        return self.day
+
+
+class Routine(models.Model):
     day = models.IntegerField("day")
-
-    def get_date(self):
-        today = datetime.today()
-        delta = (self.day - today.weekday()) % 7
-        date = datetime.today() + timedelta(days=delta)
-        return date
+    start_time = models.TimeField("start time")
+    end_time = models.TimeField("end time")
 
     def get_day(self):
         return self.day
+
+    def get_start(self):
+        return self.start_time
+
+    def get_end(self):
+        return self.end_time
--- mysite/tasks/scheduler.py
+++ mysite/tasks/scheduler.py
@@ -1,30 +1,70 @@
-import datetime
+from datetime import datetime, timedelta
 from django import timezone
-from .models import Task, Event, Routine
+from .models import Task, Event, Routine, TimeChunk
 
 
-class TimeChunk():
-    def __init__(self, obj):
-        self.name = obj.get_name()
-        self.date = obj.get_day()
-        self.start = datetime.combine(date=self.date, time=obj.get_start())
-        self.end = datetime.combine(date=self.date, time=obj.get_end())
+class TimeSlot():
+    def __init__(self, start, end, free):
+        self.start = start
+        self.end = end
+        self.free = free
 
 
-class AvailableTime(TimeChunk):
-    name = "free time"
+
+def make_event_from_task(task, date, task_start):
+    task_end = task_start + task.time_estimate
+    return Event(purpose="task",
+                 title=task.title,
+                 date=date,
+                 start_time=task_start,
+                 end_time=task_end)
 
 
-def make_schedule(day):
+def make_event_from_routine(routine, date):
+    return Event(purpose="routine",
+                 title="routine",
+                 date=date,
+                 start_time=routine.get_start(),
+                 end_time=routine.get_end())
+
+
+#class AllocatedTimeChunk(TimeChunk): def __init__(self, obj):
+#        self.name = obj.get_name()
+#        self.date = obj.get_day()
+#        self.start = datetime.combine(date=self.date, time=obj.get_start())
+#        self.end = datetime.combine(date=self.date, time=obj.get_end())
+#
+#
+#class AvailableTime(TimeChunk):
+#    name = "free time"
+#
+#    def __init__(self, date, start, end):
+#        self.date = date
+#        self.start = start
+#        self.end = end
+
+
+def get_date_from_weekday(day):
+    today = datetime.today()
+    delta = (day - today.weekday()) % 7
+    date = datetime.today() + timedelta(days=delta)
+    return date
+
+
+def clean_task_and_routine_events(date):
+    Event.objects.filter(date=date).exclude(purpose="event").delete()
+
+
+def update_schedule(day):
+    date = get_date_from_weekday(day)
+    clean_task_and_routine_events(date)
+
     routine = Routine.objects.filter(day=day)[0]
-    RoutineTC = TimeChunk(routine)
-    date = RoutineTC.date
-    events = Event.objects.filter(date=date).order_by("-start_time")
+    tasks = Task.objects.filter(done=False).order_by("-due_date",
+                                                     "-time_estimate")
+    events = Event.objects.filter(date=date).filter(purpose="event").order_by("-start_time")
 
-    event_tc_list = []
+    time_slots = []
+
     for event in events:
-        EventTC = TimeChunk(Event)
-        event_tc_list.append(EventTC)
-
-    tasks = Task.objects.filter(done=False).order_by("-due_date", "-time_estimate")
-
+        
