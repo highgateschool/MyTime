1a624fc7984070f7a64e75ce62181ad101326c5e
Max Stupple <max@stupple.org>
2020-01-20 12:21:12 +0000

    Progress on design, and documenting scheduler

--- mysite/tasks/scheduler.py
+++ mysite/tasks/scheduler.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 from .models import Task, Event, Routine, TimeSlot
 
 
+# We'll need to be able to figure out a concrete date to place routine events on
+# by deriving it from the current date and their assigned weekday
 def get_date_from_weekday(day):
     today = datetime.today()
     delta = (day - today.weekday()) % 7
@@ -11,25 +13,39 @@ def get_date_from_weekday(day):
     return date
 
 
-def clean_task_and_routine_events(date):
+# When we run the scheduling algorithm, we'll want to clean out any time slots
+# from last time
+def clean_time_slots(date):
     TimeSlot.objects.filter(date=date).delete()
 
 
+# Here's the scheduler, it runs based on a given weekday rather than a date.
+# This makes things easier
 def update_schedule(day):
+    # Get the date and clean out time slots
     date = get_date_from_weekday(day)
-    clean_task_and_routine_events(date)
+    clean_time_slots(date)
 
-    routines = Routine.objects.filter(day=day)
-    events = Event.objects.filter(date=date).order_by("-start_time")
+    # Get all events and routine events for today, ordered by start time
+    routines = Routine.objects.filter(day=day).order_by("start_time")
+    events = Event.objects.filter(date=date).order_by("start_time")
 
-    all_events = sorted(chain(routines, events),
-                        key=lambda instance: instance.start_time)
+    # Sort them together
+    all_events = sorted(
+        chain(routines, events), key=lambda instance: instance.start_time
+    )
 
-    tasks = Task.objects.filter(done=False).order_by("due_date",
-                                                     "time_estimate")
+    # Get all the tasks, ordered by due date, time estimate and descending priority level
+    tasks = Task.objects.filter(done=False).order_by(
+        "due_date", "time_estimate", "-priority"
+    )
 
+    # Convert the iterable into a list, this is easier to handle and we can remove tasks
+    # from the list once they have been allocated a time slot
     task_list = list(tasks)
 
+    # Initialise an empty list for holding the time slots, we'll write them all to the
+    # database at the end
     time_slots = []
 
     print("Begin tracing contents of time_slots")
@@ -38,26 +54,31 @@ def update_schedule(day):
         if isinstance(item, Event):
             print("Appending event...")
             time_slots.append(
-                TimeSlot(date=date,
-                         start_time=item.get_start(),
-                         end_time=item.get_end(),
-                         associated_type="E",
-                         associated_event=item))
+                TimeSlot(
+                    date=date,
+                    start_time=item.get_start(),
+                    end_time=item.get_end(),
+                    associated_type="E",
+                    associated_event=item,
+                )
+            )
             print(f"Contents: {time_slots}")
 
         elif isinstance(item, Routine):
             print("Appending routine...")
             time_slots.append(
-                TimeSlot(date=date,
-                         start_time=item.get_start(),
-                         end_time=item.get_end(),
-                         associated_type="R",
-                         associated_routine=item))
+                TimeSlot(
+                    date=date,
+                    start_time=item.get_start(),
+                    end_time=item.get_end(),
+                    associated_type="R",
+                    associated_routine=item,
+                )
+            )
             print(f"Contents: {time_slots}")
 
         else:
-            raise Exception(
-                "Looks like we have a problem with creating the TimeSlots.")
+            raise Exception("Looks like we have a problem with creating the TimeSlots.")
 
         print("Done with adding events/routines.")
         print(f"Contents: {time_slots}")
@@ -68,26 +89,30 @@ def update_schedule(day):
         for task in task_list:
             prev = time_slots[pos - 1]
             curr = time_slots[pos]
-            tdelta = datetime.combine(date,
-                                      curr.get_start()) - datetime.combine(
-                                          date, prev.get_end())
+            tdelta = datetime.combine(date, curr.get_start()) - datetime.combine(
+                date, prev.get_end()
+            )
             if tdelta > task.time_estimate:
                 start = prev.get_end()
-                end = (datetime.combine(date, prev.get_end()) +
-                       task.time_estimate).time()
+                end = (
+                    datetime.combine(date, prev.get_end()) + task.time_estimate
+                ).time()
                 print("Adding task...")
                 time_slots.insert(
                     pos,
-                    TimeSlot(date=date,
-                             start_time=start,
-                             end_time=end,
-                             associated_type="T",
-                             associated_task=task))
+                    TimeSlot(
+                        date=date,
+                        start_time=start,
+                        end_time=end,
+                        associated_type="T",
+                        associated_task=task,
+                    ),
+                )
                 print(f"Contents: {time_slots}")
                 task_list.remove(task)
                 if pos < len(time_slots):
                     pos += 1
-        pos += 1
+                    pos += 1
 
     for item in time_slots:
         item.save()
